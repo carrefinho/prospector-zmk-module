12f59e0dc1a74282e795a6a14e4c268fb07fed19
carrefinho <carrefinho@proton.me>
2025-12-23 13:50:18 -0500

    operator: add caps word indication

--- boards/shields/prospector_adapter/src/layouts/operator/modifier_indicator.c
+++ boards/shields/prospector_adapter/src/layouts/operator/modifier_indicator.c
@@ -1,6 +1,9 @@
 #include "modifier_indicator.h"
 
 #include <zmk/display.h>
+#ifdef CONFIG_DT_HAS_ZMK_BEHAVIOR_CAPS_WORD_ENABLED
+#include <zmk/events/caps_word_state_changed.h>
+#endif
 #include <zmk/events/keycode_state_changed.h>
 #include <zmk/event_manager.h>
 #include <zmk/hid.h>
@@ -13,7 +16,14 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 struct modifier_indicator_state {
     bool mods[4];
+#ifdef CONFIG_DT_HAS_ZMK_BEHAVIOR_CAPS_WORD_ENABLED
+    bool caps_word;
+#endif
 };
+
+#ifdef CONFIG_DT_HAS_ZMK_BEHAVIOR_CAPS_WORD_ENABLED
+static bool caps_word_active = false;
+#endif
 
 static void set_modifier_color(lv_obj_t *label, bool active) {
     lv_color_t color = active ? lv_color_hex(DISPLAY_COLOR_MOD_ACTIVE)
@@ -26,16 +36,35 @@ static void modifier_indicator_update_cb(struct modifier_indicator_state state) 
     SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
         for (int i = 0; i < 4; i++) {
             enum modifier_type type = modifier_order_get(i);
+#ifdef CONFIG_DT_HAS_ZMK_BEHAVIOR_CAPS_WORD_ENABLED
+            if (type == MOD_TYPE_SHIFT && state.caps_word) {
+                lv_obj_set_style_text_color(widget->mod_labels[i],
+                    lv_color_hex(DISPLAY_COLOR_MOD_CAPS_WORD), 0);
+                continue;
+            }
+#endif
             set_modifier_color(widget->mod_labels[i], state.mods[type]);
         }
     }
 }
 
 static struct modifier_indicator_state modifier_indicator_get_state(const zmk_event_t *eh) {
+#ifdef CONFIG_DT_HAS_ZMK_BEHAVIOR_CAPS_WORD_ENABLED
+    if (eh != NULL) {
+        const struct zmk_caps_word_state_changed *ev = as_zmk_caps_word_state_changed(eh);
+        if (ev != NULL) {
+            caps_word_active = ev->active;
+        }
+    }
+#endif
+
     zmk_mod_flags_t mods = zmk_hid_get_explicit_mods();
 
     struct modifier_indicator_state state = {
         .mods = {false, false, false, false},
+#ifdef CONFIG_DT_HAS_ZMK_BEHAVIOR_CAPS_WORD_ENABLED
+        .caps_word = caps_word_active,
+#endif
     };
 
     state.mods[MOD_TYPE_GUI] = (mods & (MOD_LGUI | MOD_RGUI)) != 0;
@@ -49,6 +78,10 @@ static struct modifier_indicator_state modifier_indicator_get_state(const zmk_ev
 ZMK_DISPLAY_WIDGET_LISTENER(widget_modifier_indicator, struct modifier_indicator_state,
                             modifier_indicator_update_cb, modifier_indicator_get_state)
 ZMK_SUBSCRIPTION(widget_modifier_indicator, zmk_keycode_state_changed);
+
+#ifdef CONFIG_DT_HAS_ZMK_BEHAVIOR_CAPS_WORD_ENABLED
+ZMK_SUBSCRIPTION(widget_modifier_indicator, zmk_caps_word_state_changed);
+#endif
 
 static lv_obj_t *create_separator(lv_obj_t *parent) {
     lv_obj_t *sep = lv_obj_create(parent);
