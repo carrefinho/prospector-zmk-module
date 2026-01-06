9b1909810622f4358adbb690c534b1685819b123
carrefinho <carrefinho@proton.me>
2026-01-05 20:35:24 -0500

    all screens: rename default to classic, wind_map to field, rounded_grid to radii

--- boards/shields/prospector_adapter/src/custom_status_screen.c
+++ boards/shields/prospector_adapter/src/custom_status_screen.c
@@ -1,11 +1,11 @@
 #include <lvgl.h>
 
-#if defined(CONFIG_PROSPECTOR_STATUS_SCREEN_DEFAULT)
-#include "layouts/default/status_screen.c"
-#elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_ROUNDED_GRID)
-#include "layouts/rounded_grid/status_screen.c"
-#elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_WIND_MAP)
-#include "layouts/wind_map/status_screen.c"
+#if defined(CONFIG_PROSPECTOR_STATUS_SCREEN_CLASSIC)
+#include "layouts/classic/status_screen.c"
+#elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_RADII)
+#include "layouts/radii/status_screen.c"
+#elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_FIELD)
+#include "layouts/field/status_screen.c"
 #elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_OPERATOR)
 #include "layouts/operator/status_screen.c"
 #else
